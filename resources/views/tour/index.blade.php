ed48607621f27d7bca8dad5e9dd8640e3684f9c0
anjas putra <anjasputra012@gmail.com>
2026-04-26 14:19:37 +0700

    'd'

--- resources/views/tour/index.blade.php
+++ resources/views/tour/index.blade.php
@@ -336,6 +336,23 @@
             box-shadow: 0 0 15px rgba(99, 102, 241, 0.6);
         }
 
+        /* Pseudo-fullscreen for iOS/Safari */
+        .pseudo-fullscreen, .pseudo-fullscreen body {
+            position: fixed !important;
+            top: 0 !important;
+            left: 0 !important;
+            width: 100vw !important;
+            height: 100vh !important;
+            z-index: 999999 !important;
+            overflow: hidden !important;
+        }
+
+        #panolens-container {
+            width: 100%;
+            height: 100%;
+            background-color: #000;
+        }
+
         .scene-card img {
             width: 100%;
             height: 100%;
@@ -1621,6 +1638,14 @@
                     const lat0 = parseFloat(startSceneData?.initial_lat ?? 0);
                     if (lon0 !== 0 || lat0 !== 0) {
                         setTimeout(() => _applyInitialView(lon0, lat0), 300);
+                    }
+
+                    // Auto pseudo-fullscreen for iOS
+                    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+                    if (isIOS) {
+                        document.documentElement.classList.add('pseudo-fullscreen');
+                        document.getElementById('toggle-fullscreen')?.classList.add('btn-active');
+                        window.dispatchEvent(new Event('resize'));
                     }
                 });
             } else {
